dc0c17b966892001c1f93f63300d6365b6e7154e
deathdogg <47575628+deathdogg@users.noreply.github.com>
2024-02-10 20:42:12 -0800

    - Integrated Swift Data the Widget extension to get information directly from the user data storeinstead of using user defaults

--- CCWidget/CCWidget.swift
+++ CCWidget/CCWidget.swift
@@ -4,28 +4,43 @@
 //
 //  Created by Ricardo Herrera on 2/10/24.
 //
-
+import SwiftData
 import WidgetKit
 import SwiftUI
 
 struct Provider: TimelineProvider {
-	@AppStorage("lastActivity", store: UserDefaults(suiteName: "group.compass-companion")!) private var lastActivity: String?
-	@AppStorage("lastActivityDate", store: UserDefaults(suiteName: "group.compass-companion")!) private var lastActivityDate: TimeInterval?
-	func placeholder(in context: Context) -> CCWEntry {
-		CCWEntry(date: Date(), activity: "Park")
+	@MainActor
+	private func getLogEntries() -> [LogEntry] {
+		var activities: [LogEntry] = []
+		var activitiesDescriptor = FetchDescriptor<LogEntry>(sortBy: [SortDescriptor(\LogEntry.date)])
+		guard let moc = try? ModelContainer(for: LogEntry.self) else {
+			return activities
+		}
+		do {
+			activities = try moc.mainContext.fetch(activitiesDescriptor)
+		} catch {
+			print(error)
+		}
+		return activities
 	}
-
+	func placeholder(in context: Context) -> CCWEntry {
+		CCWEntry(date: Date(), activities:
+		[
+			LogEntry(activity: .park, date: .now)
+		])
+	}
+@MainActor
 	func getSnapshot(in context: Context, completion: @escaping (CCWEntry) -> ()) {
-		let entry = CCWEntry(date: Date(timeIntervalSince1970: lastActivityDate ?? Date.now.timeIntervalSince1970), activity: lastActivity ?? "Park")
+
+		let entry = Entry(date: .now, activities: self.getLogEntries())
 		completion(entry)
 	}
-
+@MainActor
 	func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
 		var entries: [CCWEntry] = []
 
 		// Generate a timeline consisting of five entries an hour apart, starting from the current date.
-		let entryDate = Date(timeIntervalSince1970: lastActivityDate ?? Date.now.timeIntervalSince1970)
-			let entry = CCWEntry(date: entryDate, activity: lastActivity ?? "Park")
+		let entry = CCWEntry(date: .now, activities: self.getLogEntries())
 			entries.append(entry)
 		let timeline = Timeline(entries: entries, policy: .atEnd)
 		completion(timeline)
@@ -34,19 +49,20 @@ struct Provider: TimelineProvider {
 
 struct CCWEntry: TimelineEntry {
 	let date: Date
-	let activity: String
+	let activities: [LogEntry]
 }
 
 struct CCWidgetEntryView : View {
 	var entry: Provider.Entry
+	var lastActivity: LogEntry {
+		entry.activities.last ?? LogEntry(activity: .park, date: 	.now)
+	}
 	var body: some View {
 		VStack {
 			Text("Time:")
-			Text(entry.date.formatted())
+			Text(lastActivity.date.formatted())
 			Text("Activity:")
-			Text(entry.activity)
-				.onAppear {
-				}
+			Text(lastActivity.activity.rawValue)
 		}
 	}
 }
@@ -56,9 +72,7 @@ struct CCWidget: Widget {
 	var body: some WidgetConfiguration {
 		StaticConfiguration(kind: kind, provider: Provider()) {
 			entry in
-
 			if #available(iOS 17.0, *) {
-
 				CCWidgetEntryView(entry: entry)
 					.containerBackground(.fill.tertiary, for: .widget)
 			} else {
@@ -67,7 +81,6 @@ struct CCWidget: Widget {
 					.background()
 			}
 		}
-
 		.configurationDisplayName("Compass Companion Widget")
 		.description("This is an example widget.")
 	}
